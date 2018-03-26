eb7481e99d3fae71061b5ca9c81d5b846e1f8550
Stu Thompson <stuart@stuartthompson.net>
2018-03-26 11:49:42 -0700

    Adds window size and border render functions.
    Adds support function to retrieve termbox window size.
    Adds a method to render a border around a pane.

--- io/Render.go
+++ io/Render.go
@@ -21,6 +21,15 @@ import (
 	"github.com/nsf/termbox-go"
 )
 
+const (
+	BorderRuneHorizontal = rune('\u2550')
+	BorderRuneVertical = rune('\u2551')
+	BorderRuneCornerTopLeft = rune('\u2554')
+	BorderRuneCornerTopRight = rune('\u2557')
+	BorderRuneCornerBottomLeft = rune('\u255A')
+	BorderRuneCornerBottomRight = rune('\u255D')
+)
+
 // ClearScreen ...
 // Clears the screen using a specified color.
 func ClearScreen(bgColor int) {
@@ -31,6 +40,36 @@ func ClearScreen(bgColor int) {
 // Flush render commands.
 func Flush() {
 	termbox.Flush()
+}
+
+func GetWindowSize() (width int, height int) {
+	return termbox.Size()
+}
+
+// RenderPaneBorder ...
+// Renders a border for a window pane.
+func RenderPaneBorder(x int, y int, width int, height int, fgColor int, bgColor int) {
+	// Render the corners
+	termbox.SetCell(x, y, BorderRuneCornerTopLeft, termbox.Attribute(fgColor), termbox.Attribute(bgColor))
+	termbox.SetCell(x + width, y, BorderRuneCornerTopRight, termbox.Attribute(fgColor), termbox.Attribute(bgColor))
+	termbox.SetCell(x, y + height, BorderRuneCornerBottomLeft, termbox.Attribute(fgColor), termbox.Attribute(bgColor))
+	termbox.SetCell(x + width, y + height, BorderRuneCornerBottomRight, termbox.Attribute(fgColor), termbox.Attribute(bgColor))
+	// Render top border
+	for ix := 1; ix < width; ix++ {
+		termbox.SetCell(x + ix, y, BorderRuneHorizontal, termbox.Attribute(fgColor), termbox.Attribute(bgColor))
+	}
+	// Render bottom border
+	for ix := 1; ix < width; ix++ {
+		termbox.SetCell(x + ix, y + height, BorderRuneHorizontal, termbox.Attribute(fgColor), termbox.Attribute(bgColor))
+	}
+	// Render left border
+	for iy := 1; iy < height; iy++ {
+		termbox.SetCell(x, y + iy, BorderRuneVertical, termbox.Attribute(fgColor), termbox.Attribute(bgColor))
+	}
+	// Render right border
+	for iy := 1; iy < height; iy++ {
+		termbox.SetCell(x + width, y + iy, BorderRuneVertical, termbox.Attribute(fgColor), termbox.Attribute(bgColor))
+	}
 }
 
 // RenderText ...
