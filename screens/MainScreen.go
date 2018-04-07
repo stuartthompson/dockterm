f004db24d7c533eeb330650ba058d5dd84dee024
Stu Thompson <stuart@stuartthompson.net>
2018-04-07 12:06:17 -0700

    Main screen now lists all Docker containers.
    Some render problems with certain characters (like ellipsis).

--- screens/MainScreen.go
+++ screens/MainScreen.go
@@ -18,6 +18,9 @@
 package screens
 
 import (
+	"log"
+	"os/exec"
+	"strings"
 	"github.com/stuartthompson/dockterm/io"
 )
 
@@ -32,5 +35,20 @@ func (s *MainScreen) Render() {
 	width, height := io.GetWindowSize()
 	io.RenderPaneBorder(0, 0, width - 1, height - 1, 82, 0)
 	io.RenderText("Main", 1, 1, 255, 0)
+
+	// List running containers
+	ctrs := s.getRunningContainers()
+	ctrList := strings.Split(ctrs, "\n")
+	for ix, ctr := range ctrList {
+		io.RenderText(ctr, 1, 3 + ix, 255, 0)
+	}
 	io.Flush()
+}
+
+func (s *MainScreen) getRunningContainers() string {
+	output, err := exec.Command("/usr/bin/docker", "ps", "-a").Output()
+	if err != nil {
+		log.Fatal(err)
+	}
+	return string(output)
 }
