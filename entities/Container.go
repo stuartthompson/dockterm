db9ddea9acf74d288b22eefec0587e0cd76a17e3
Stu Thompson <stuart@stuartthompson.net>
2018-05-10 09:31:14 -0700

    Adds more properties to the Container struct.
    Unsure about ExecIDs as it is returning null in docker inspect.
    Need to go read the documentation to understand the structure of
    this field.

--- entities/Container.go
+++ entities/Container.go
@@ -3,11 +3,24 @@ package entities
 // Container ...
 // Represents a Docker container.
 type Container struct {
-	ID      string
-	Created string
-	Path    string
-	Args    []string
-	State   ContainerState
+	ID              string
+	Created         string
+	Path            string
+	Args            []string
+	State           ContainerState
+	Image           string
+	ResolvConfPath  string
+	HostnamePath    string
+	HostsPath       string
+	LogPath         string
+	Name            string
+	RestartCount    int
+	Driver          string
+	Platform        string
+	MountLabel      string
+	ProcessLabel    string
+	AppArmorProfile string
+	ExecIDs         []string
 }
 
 // ContainerState ...
