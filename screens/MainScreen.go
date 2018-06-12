ff7fa474c4046858f4b62aa2cf95d0a03f686e83
Stu Thompson <stuart@stuartthompson.net>
2018-06-12 11:22:09 -0700

    Removes redundant whitespace in header.

--- screens/MainScreen.go
+++ screens/MainScreen.go
@@ -13,7 +13,7 @@
 // GNU General Public License for more details.
 
 // You should have received a copy of the GNU General Public License
-// along with Dockterm.  If not, see <http://www.gnu.org/licenses/>.
+// along with Dockterm. If not, see <http://www.gnu.org/licenses/>.
 
 package screens
 
